0889d3e9968cb547a0f439016c25f4375e4a72ed
pastperfeckt <pastperfeckt@yahoo.de>
2023-05-24 12:46:10 +0200

    site url changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pastperfeckt.github.io/interactiveCV/",
+  site: "https://pastperfeckt.github.io/interactiveCV",
   integrations: [
     tailwind(),
     sitemap({

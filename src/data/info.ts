ab12753f245aef18de87ca5b5d777ec337776cb4
pastperfeckt <pastperfeckt@yahoo.de>
2023-05-26 23:14:36 +0200

    content optimized with AI and phone button added

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,12 +2,17 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Paul Pellekoorne",
   jobDescription: "Web Developer",
-  about: `I'm a project manager based in Munich, Germany.
-   I feel at home in the field of mobility. I love teamwork, programming, automation and creativity.
-   I am grateful that after graduating I was able to experience, accompany, support and help steer the development of TTS Europe from the very beginning.
-   Afterwards, I was able to gain experience as a project manager at Accenture, where I took responsibility for three projects.
-   Since June 2023 I'm driving the IT transformation of Allane SE.
+  about: `Dynamic and results-driven Project Manager with expertise in the field of mobility.
+  Passionate about teamwork, programming, automation, and creativity.
+  Demonstrated ability to steer projects from inception to successful completion.
+  Committed to driving business growth and leveraging innovative solutions to optimize processes.
   `,
+  //about: `I'm a project manager based in Munich, Germany.
+  //I feel at home in the field of mobility. I love teamwork, programming, automation and creativity.
+  //I am grateful that after graduating I was able to experience, accompany, support and help steer the development of TTS Europe from the very beginning.
+  //Afterwards, I was able to gain experience as a project manager at Accenture, where I took responsibility for three projects.
+  //Since June 2023 I'm driving the IT transformation of Allane SE.
+  //`,
 
   experience: [
     {
@@ -16,8 +21,9 @@ export const info = {
       startDate: "Jun 2023",
       endDate: "Present",
       description: [
-        "✓ Program Manager of the IT Modernization Program",
-        "✓ Coordination of modernization projects and design of objectives and processes",
+        "✓ Lead the IT Modernization Program, overseeing the coordination of multiple modernization projects and designing objectives and processes.",
+        "✓ Drive strategic initiatives to enhance operational efficiency and support business growth.",
+        "✓ Collaborate with cross-functional teams to ensure alignment with project goals and deliverables.",
       ],
     },
 
@@ -27,11 +33,16 @@ export const info = {
       startDate: "May 2021",
       endDate: "May 2023",
       description: [
-        "✓ Multi-project management of agile software development projects in the automotive industry",
-        "✓ Project lead of the content management of an eShop",
-        "✓ Measured increase in customer satisfaction, employee satisfaction, product quality and revenue",
-        "✓ Trainer of courses on project management, teamwork and motivation",
+        "✓ Managed and delivered agile software development projects in the automotive industry, ensuring successful outcomes within budget and timeline constraints.",
+        "✓ Led the content management of an eShop, resulting in improved customer satisfaction, employee satisfaction, product quality, and revenue.",
+        "✓ Conducted training sessions on project management, teamwork, and motivation.",
       ],
+      //description: [
+      //  "✓ Multi-project management of agile software development projects in the automotive industry",
+      //  "✓ Project lead of the content management of an eShop",
+      //  "✓ Measured increase in customer satisfaction, employee satisfaction, product quality and revenue",
+      //  "✓ Trainer of courses on project management, teamwork and motivation",
+      //],
     },
 
     {
@@ -40,12 +51,16 @@ export const info = {
       startDate: "Jan 2016",
       endDate: "Apr 2021",
       description: [
-        "✓ Product Owner and Project Manager for emulation-based traffic light phase forecasts",
-        "✓ Realization of the first commercial V2I service in Germany",
-        "✓ Development of a traffic light phase forecast with emulators for German ECUs",
-        "✓ Conceptual Design, Development, Testing, Deployment, Customer Relations and IT/Operations",
-        "✓ R&D projects, patent holder"
+        "✓ Spearheaded the development of emulation-based traffic light phase forecasts, resulting in the realization of the first commercial V2I service in Germany.",
+        "✓ Successfully managed end-to-end project lifecycle, including conceptual design, development, testing, deployment, customer relations, and IT/Operations.",
+        "✓ Contributed to R&D projects and held patents in the field.",
       ],
+      //description: [
+      //  "✓ Product Owner and Project Manager for emulation-based traffic light phase forecasts",
+      //  "✓ Realization of the first commercial V2I service in Germany",
+      //  "✓ Development of a traffic light phase forecast with emulators for German ECUs",
+      //  "✓ Conceptual Design, Development, Testing, Deployment, Customer Relations and IT/Operations",
+      //  "✓ R&D projects, patent holder"
     },
 
     {
@@ -54,9 +69,8 @@ export const info = {
       startDate: "Sep 2014",
       endDate: "Feb 2015",
       description: [
-        "✓ Teaching Higher Mathematics for Engineers",
-        "✓ Management of the e-learning platform",
-        "✓ 3D printing of three-dimensional functions"
+        "✓ Instructed Higher Mathematics for Engineers, while managing the e-learning platform.",
+        "✓ Conducted research and 3D printing of three-dimensional functions.",
       ],
     },
   ],
@@ -80,10 +94,10 @@ export const info = {
       startDate: "2010",
       endDate: "2015",
       description: [
-        "✓ Reviewers in over 30 accreditation and evaluation procedures",
-        "✓ Expert committee member of the accreditation agency ASIIN e.V.",
-        "✓ Organizer and assessor in aptitude assessment procedures",
-        "✓ Member of appeal committees"
+        "✓ Participated as a reviewer in over 30 accreditation and evaluation procedures.",
+        "✓ Served as an expert committee member of the accreditation agency ASIIN e.V.",
+        "✓ Organized and assessed aptitude assessment procedures.",
+        "✓ Member of appeal committees."
       ],
     },
     {
@@ -92,9 +106,9 @@ export const info = {
       startDate: "2000",
       endDate: "2009",
       description: [
-        "✓ Piano lessons, from 2006 excellence class",
+        "✓ Excellence class in piano lessons since 2006",
         "✓ Additional commitment as a tutor and mediator",
-        "✓ Advanced Courses Mathematics and Music",
+        "✓ Advanced courses in mathematics and music",
       ],
     },
   ],
@@ -103,6 +117,7 @@ export const info = {
     facebook: "https://www.facebook.com/paul.pellekoorne",
     email: "mailto:paul@pellekoorne.de",
     linkedin: "https://www.linkedin.com/in/paulpellekoorne/",
+    phone: "tel:004915253484139",
   },
 
   projects: [

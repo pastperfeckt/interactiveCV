950227992cf968516de47ed4c350e2b643820e57
pastperfeckt <pastperfeckt@yahoo.de>
2023-05-26 11:15:32 +0200

    content adapted

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,134 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  name: "Paul Pellekoorne",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `I'm a project manager based in Munich, Germany.
+   I feel at home in the field of mobility. I love teamwork, programming, automation and creativity.
+   I am grateful that after graduating I was able to experience, accompany, support and help steer the development of TTS Europe from the very beginning.
+   Afterwards, I was able to gain experience as a project manager at Accenture, where I took responsibility for three projects.
+   Since June 2023 I'm driving the IT transformation of Allane SE.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Allane SE",
+      location: "Pullach, Germany",
+      startDate: "Jun 2023",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "✓ Program Manager of the IT Modernization Program",
+        "✓ Coordination of modernization projects and design of objectives and processes",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Accenture",
+      location: "Munich, Germany",
+      startDate: "May 2021",
+      endDate: "May 2023",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "✓ Multi-project management of agile software development projects in the automotive industry",
+        "✓ Project lead of the content management of an eShop",
+        "✓ Measured increase in customer satisfaction, employee satisfaction, product quality and revenue",
+        "✓ Trainer of courses on project management, teamwork and motivation",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Traffic Technology Services Europe",
+      location: "Munich, Germany & Portland, OR",
+      startDate: "Jan 2016",
+      endDate: "Apr 2021",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "✓ Product Owner and Project Manager for emulation-based traffic light phase forecasts",
+        "✓ Realization of the first commercial V2I service in Germany",
+        "✓ Development of a traffic light phase forecast with emulators for German ECUs",
+        "✓ Conceptual Design, Development, Testing, Deployment, Customer Relations and IT/Operations",
+        "✓ R&D projects, patent holder"
+      ],
+    },
+
+    {
+      name: "German University of Technology",
+      location: "Muscat, Oman",
+      startDate: "Sep 2014",
+      endDate: "Feb 2015",
+      description: [
+        "✓ Teaching Higher Mathematics for Engineers",
+        "✓ Management of the e-learning platform",
+        "✓ 3D printing of three-dimensional functions"
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "M.Sc. Environmental Engineering (1.4)",
+      location: "Technical University of Munich",
+      startDate: "2009",
+      endDate: "2015",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "✓ Master thesis: Predictive Impact Evaluation on Urban Traffic Guidance Measures (1.3)",
+        "✓ Study project at DriveNow on dynamic pricing systems for free-floating car sharing services (1.0)",
+        "✓ Bachelor Thesis: Building Information Modeling (1.0)",
+        "✓ Since 2011 Tutor of Higher Mathematics I and II"
       ],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
+      name: "Additional commitment",
+      location: "Worldwide",
+      startDate: "2010",
+      endDate: "2015",
+      description: [
+        "✓ Reviewers in over 30 accreditation and evaluation procedures",
+        "✓ Expert committee member of the accreditation agency ASIIN e.V.",
+        "✓ Organizer and assessor in aptitude assessment procedures",
+        "✓ Member of appeal committees"
+      ],
     },
     {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      name: "Baccalaureate / Abitur (2.4)",
+      location: "Munich, Germany",
+      startDate: "2000",
+      endDate: "2009",
+      description: [
+        "✓ Piano lessons, from 2006 excellence class",
+        "✓ Additional commitment as a tutor and mediator",
+        "✓ Advanced Courses Mathematics and Music",
+      ],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    facebook: "https://www.facebook.com/paul.pellekoorne",
+    email: "mailto:paul@pellekoorne.de",
+    linkedin: "https://www.linkedin.com/in/paulpellekoorne/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Multimodal Mobility App",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      //display ratio 3:2
+      thumbnail: "/assets/images/Urby_Frame2.jpg",
+      githubUrl: "",
+      liveUrl: "https://www.accenture.com/us-en/industries/automotive-index?c=acn_glb_foundationalbaleader_13582181&n=smc_0423#block-bmw-groups-urby-mobility",
+    },
+
+    {
+      title: "Traffic light prediction",
+      isFeatured: true,
+      thumbnail: "/assets/images/AudiTLI4.jpg",
+      githubUrl: "",
+      liveUrl: "https://www.instagram.com/reel/CfGFbKJF0Kp/",
+    },
+
+    {
+      title: "Dynamic Pricing in Car-Sharing",
+      isFeatured: false,
+      thumbnail: "/assets/images/drivenow.png",
+      githubUrl: "",
+      liveUrl: "https://www.iphone-ticker.de/driven-save-drivenow-optimiert-app-fuer-neues-tarifmodell-78977/",
     },
   ],
 };
